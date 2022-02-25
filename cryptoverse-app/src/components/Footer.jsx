87817d33edd20399395a8824edc4796d474ff043
Zeeshan Ahmad Khan <nwtnzack003@gmail.com>
2022-02-25 11:56:39 +0530

    completed cryptoverse app

--- cryptoverse-app/src/components/Footer.jsx
+++ cryptoverse-app/src/components/Footer.jsx
@@ -1,10 +1,13 @@
+import { FaGithub } from 'react-icons/fa'
+
 import classes from '../components/Footer.module.css';
 
 function Footer() {
     return (
         <div className={classes.footer}>
-            <h3>Copyright&copy; {new Date().getFullYear()}</h3>
+            <a href="https://github.com/zeeshan-ahmad-khan/react-projects/tree/main/cryptoverse-app">If you like it. Give it a STAR <FaGithub /></a>
             <h5>Made by Zeeshan with Coolness 🧊</h5>
+            <h3>Copyright&copy; {new Date().getFullYear()} 😂</h3>
             <div className={classes.attrib}>
                 <h3>Powered by</h3>
                 <img src="https://rapidapi.com/static-assets/default/logo-85082ac1-9f07-481f-97c7-538f38e56420.svg" alt="Rapid Api" />
